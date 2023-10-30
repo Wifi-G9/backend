74403f04ef490f57744ec2ce1795a20759e77d7c
Sipos-Lucas-George <lucas.sipos@yahoo.com>
2023-10-30 18:31:34 +0200

    main: Added database information(ask for .env variables)
    Signed-off-by: Sipos-Lucas-George <lucas.sipos@yahoo.com>

--- backend/tsma/settings.py
+++ backend/tsma/settings.py
@@ -92,6 +92,7 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
+        "OPTIONS": {"driver": "ODBC Driver 18 for SQL Server", }
     }
 }
 
