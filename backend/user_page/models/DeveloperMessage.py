dedaad74c8ab0c677675402da53d7d69bd14a079
SICOE <sergiu.sicoe@stud.ubbcluj.ro>
2023-11-01 16:23:16 +0200

    remove on delete cascade + changed fields to nullable + make user email unique

--- backend/user_page/models/DeveloperMessage.py
+++ backend/user_page/models/DeveloperMessage.py
@@ -7,4 +7,4 @@ class DeveloperMessage(models.Model):
     id = models.AutoField(primary_key=True)
     message = models.CharField(max_lenght=500)
     timestamp = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True)
--- backend/user_page/models/SearchHistory.py
+++ backend/user_page/models/SearchHistory.py
@@ -5,7 +5,7 @@ from backend.user_page.models.User import User
 
 class SearchHistory(models.Models):
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     search_query = models.CharField(max_lenght=100)
 
