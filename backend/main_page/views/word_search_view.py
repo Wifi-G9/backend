59a1f9c54af4c1dbd877025481322655afa2cc33
Sipos-Lucas-George <lucas.sipos@yahoo.com>
2023-11-12 22:24:06 +0200

    word_search_view: Added get_word_of_the_day func and extra documentation
    Signed-off-by: Sipos-Lucas-George <lucas.sipos@yahoo.com>

--- backend/main_page/views/word_search_view.py
+++ backend/main_page/views/word_search_view.py
@@ -14,14 +14,21 @@ class WordSearch(View):
         Function returning word of the day or the searched word verified by api
         :param request: Request
         :return: Response with data: dict and status: status
+        Response(
+            {
+                "word_search": "word" if word exists else "Not found"
+            },
+            status_code: int = 200 if word exists else 400
+        )
         """
         word_searched = request.query_params.get("search", None)
+        user = request.query_params.get("user", None)
 
         if word_searched is not None:
             response = requests.get(API_DICTIONARY + word_searched).json()
             if len(response) == 3:
                 return Response({"word_searched": response["title"]}, status=status.HTTP_404_NOT_FOUND)
-            SearchHistory.objects.create(user=None, search_query=word_searched)
+            SearchHistory.objects.create(user=user, search_query=word_searched)
         else:
             word_searched = self.get_word_of_the_day()
 
@@ -30,7 +37,7 @@ class WordSearch(View):
     @staticmethod
     def get_word_of_the_day() -> str:
         """
-        Fetches from database the most searched word from a week ago, and returns it, else cookie (^*^)
+        Fetches from database the most searched word from a week ago and returns it
         :return: str
         """
-        return "cookie"  # TODO implement word of the day
+        return SearchHistory.objects.latest('search_query').search_query
