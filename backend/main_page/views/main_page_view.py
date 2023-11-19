156f813bfb11ea5419ad608f9a50d9674a572c12
Sipos-Lucas-George <lucas.sipos@yahoo.com>
2023-11-19 16:56:36 +0200

    view_package: Renamed every .py without "_view" in name
    Signed-off-by: Sipos-Lucas-George <lucas.sipos@yahoo.com>

--- backend/main_page/views/main_page_view.py
+++ backend/main_page/views/main_page_view.py
@@ -3,12 +3,12 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from main_page.views.sentiment_analysis import SentimentAnalysis
+from main_page.views.sentiment_analysis_view import SentimentAnalysis
 from main_page.views.word_search_view import WordSearch
 from main_page.views.instagram_search_view import InstagramSearch
-from main_page.views.similar_trends import SimilarTrends
-from main_page.views.popular_trends import PopularTrends
-from main_page.views.interest_over_time import InterestOverTime
+from main_page.views.similar_trends_view import SimilarTrends
+from main_page.views.popular_trends_view import PopularTrends
+from main_page.views.interest_over_time_view import InterestOverTime
 from main_page.views.chatgpt_description_view import ChatGPTDescription
 
 
@@ -21,22 +21,11 @@ class MainPageView(APIView):
         popular_trends: Response = PopularTrends.as_view()(request)
         interest_over_time: Response = InterestOverTime.as_view()(request)
         sentiment_analysis: Response = SentimentAnalysis.as_view()(request)
-
-        word_searched_data = word_searched.data["word_searched"]
-        chatgpt_description: Response = ChatGPTDescription.as_view()(request, word_searched_data)
-
-        if word_searched.status_code == 404:
-            return word_searched
-
-        if chatgpt_description.status_code != 200:
-            return chatgpt_description
-
-        if sentiment_analysis.status_code == 404:
-            return sentiment_analysis
+        chatgpt_description: Response = ChatGPTDescription.as_view()(request)
 
         aggregated_data = {
             **word_searched.data,
-            # **instagram_search.data,
+            **instagram_search.data,
             **chatgpt_description.data,
             **similar_trends.data,
             **popular_trends.data,
