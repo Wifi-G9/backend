87c8392b580393af701d1cdab2ee5ef27fe51a19
Sipos-Lucas-George <lucas.sipos@yahoo.com>
2023-11-19 15:47:13 +0200

    popular_trends: Added popular trends view
    Signed-off-by: Sipos-Lucas-George <lucas.sipos@yahoo.com>

---
+++ backend/main_page/views/popular_trends.py
@@ -0,0 +1,28 @@
+from django.views import View
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from pytrends.request import TrendReq
+
+
+class PopularTrends(View):
+    @staticmethod
+    def get(request: Request) -> Response:
+        """
+        Function returning similar trends for a topic
+        :param request: Request
+        :return: Response with data: dict and status: status
+        Response(
+            {
+                "popular_trends": [str, str, str, str, str] if found
+            },
+            status_code: int = 200 if word exists else 404
+        )
+        """
+        try:
+            pytrends = TrendReq(hl='en-US', tz=360)
+            popular_trends = [trends for trends in pytrends.realtime_trending_searches(pn='US')["title"][:5]]
+        except KeyError:
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response({"popular_trends": popular_trends}, status=status.HTTP_200_OK)
