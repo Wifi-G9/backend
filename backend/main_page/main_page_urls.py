4a4b6c5f7ff579c60b769f37a0adad55f8e3eecf
Sipos-Lucas-George <lucas.sipos@yahoo.com>
2023-11-19 15:19:42 +0200

    SentimentAnalysis: bimbimbambam tested the code
    Signed-off-by: Sipos-Lucas-George <lucas.sipos@yahoo.com>

--- backend/main_page/main_page_urls.py
+++ backend/main_page/main_page_urls.py
@@ -1,5 +1,4 @@
 from main_page.views.main_page_view import MainPageView
-from main_page.views.sentiment_analysis import SentimentAnalysis
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
@@ -7,7 +6,6 @@ from main_page.views.word_search_view import WordSearch
 
 urlpatterns = [
     path('', MainPageView.as_view()),
-    path('sentiment-analysis/', SentimentAnalysis.as_view(), name='sentiment_analysis'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
