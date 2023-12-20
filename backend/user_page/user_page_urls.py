10521bf0808ea7e98df36b29ce9c4fb809f4f713
Sipos-Lucas-George <lucas.sipos@yahoo.com>
2023-12-21 00:11:35 +0200

    user_feed_view: Added the view returning a bunch of necessary regarding the user profile
    Signed-off-by: Sipos-Lucas-George <lucas.sipos@yahoo.com>

--- backend/user_page/user_page_urls.py
+++ backend/user_page/user_page_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-
+from user_page.views.user_feed_view import UserFeedView
 from user_page.views.music_suggestion_view import MusicSuggestion
 
 urlpatterns = [
+    path('', UserFeedView.as_view()),
     path('description-deezer', MusicSuggestion.as_view()),
 ]
 
